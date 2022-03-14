c9a6078c41efb9972cd2fcbeedea17e6b8ac1101
ibad81 <ibadbaig921@gmail.com>
2022-03-14 17:37:29 +0530

    Second commit

---
+++ src/Selection_sort.java
@@ -0,0 +1,19 @@
+public class Selection_sort {
+
+    int [] arr;
+    Selection_sort(){
+        int [] arr={10,5,6,2,8};
+    }
+    void Sort(){
+        int n= arr.length;
+        for (int i = 0; i <n-1 ; i++) {
+            int min_index=i;
+            for (int j = i+1; j <n ; j++) {
+                if(arr[j]<arr[min_index])
+                    min_index=j;
+            }
+            int temp=ar
+        }
+    }
+
+}
